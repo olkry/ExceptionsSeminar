e199a227967c4893b4f3743e4ec9116b76c1546c
olkry <adenos90@gmail.com>
2023-07-29 16:20:00 +0300

    Add seminar02DZ correct

--- Seminar02DZ/src/Task03/Task03.java
+++ Seminar02DZ/src/Task03/Task03.java
@@ -2,39 +2,79 @@ package Task03;
 
 import java.util.Scanner;
 
+//public class Task03 {
+//    public static String numberCheck(int number1, int number2, int number3) throws NumberOutOfRangeException,
+//            NumberSumException, DivisionByZeroException {
+//        if (number1 > 100) {
+//            throw new NumberOutOfRangeException("Первое число вне допустимого диапазона");
+//        }
+//        if (number2 < 0) {
+//            throw new NumberOutOfRangeException("Второе число вне допустимого диапазона");
+//        }
+//        if (number1 + number2 < 10) {
+//            throw new NumberSumException("Сумма первого и второго чисел слишком мала");
+//        }
+//        if (number3 == 0) {
+//            throw new DivisionByZeroException("Деление на ноль недопустимо");
+//        }
+//        else {
+//            return "Проверка пройдена успешно";
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Введите первое число(МЕНЕЕ 100): ");
+//        int number1 = Integer.parseInt(scan.next());
+//        System.out.println("Введите второе число(БОЛЕЕ 0): ");
+//        int number2 = Integer.parseInt(scan.next());
+//        System.out.println("Введите третье число(НЕ 0): ");
+//        int number3 = Integer.parseInt(scan.next());
+//        try {
+//            System.out.println(numberCheck(number1,number2,number3));
+//        } catch (NumberSumException | DivisionByZeroException | NumberOutOfRangeException e) {
+//            System.out.println("Ошибка принятых данных: " + e.getMessage());
+//        }
+//    }
+//}
+
+// Улучшеный вариант от Леонида.
 public class Task03 {
-    public static String numberCheck(int number1, int number2, int number3) throws NumberOutOfRangeException,
-            NumberSumException, DivisionByZeroException {
+
+    public static String numberCheck(int number1, int number2, int number3)
+            throws NumberOutOfRangeException, NumberSumException, DivisionByZeroException {
+
         if (number1 > 100) {
             throw new NumberOutOfRangeException("Первое число вне допустимого диапазона");
-        }
-        if (number2 < 0) {
+        } else if (number2 < 0) {
             throw new NumberOutOfRangeException("Второе число вне допустимого диапазона");
-        }
-        if (number1 + number2 < 10) {
+        } else if (number1 + number2 < 10) {
             throw new NumberSumException("Сумма первого и второго чисел слишком мала");
-        }
-        if (number3 == 0) {
+        } else if (number3 == 0) {
             throw new DivisionByZeroException("Деление на ноль недопустимо");
-        }
-        else {
+        } else {
             return "Проверка пройдена успешно";
         }
-
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите первое число(МЕНЕЕ 100): ");
-        int number1 = Integer.parseInt(scan.next());
-        System.out.println("Введите второе число(БОЛЕЕ 0): ");
-        int number2 = Integer.parseInt(scan.next());
-        System.out.println("Введите третье число(НЕ 0): ");
-        int number3 = Integer.parseInt(scan.next());
         try {
+            System.out.println("Введите первое число(МЕНЕЕ 100): ");
+            int number1 = Integer.parseInt(scan.next());
+            System.out.println("Введите второе число(БОЛЕЕ 0): ");
+            int number2 = Integer.parseInt(scan.next());
+            System.out.println("Введите третье число(НЕ 0): ");
+            int number3 = Integer.parseInt(scan.next());
+
             System.out.println(numberCheck(number1,number2,number3));
         } catch (NumberSumException | DivisionByZeroException | NumberOutOfRangeException e) {
             System.out.println("Ошибка принятых данных: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка: введено не число!");
+        } finally {
+            scan.close();
         }
     }
 }
